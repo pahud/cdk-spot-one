aac5c28ef04047628c1bdd3d085ad523507caed5
Neil Guan <guan840912@gmail.com>
2020-08-31 16:33:45 +0800

    update defaultSecurityGruop Description

--- src/spot.ts
+++ src/spot.ts
@@ -204,7 +204,7 @@ export class SpotFleet extends Resource {
    */
   private validUntil?: string;
   /**
-   * @default - open SSH 22 port Any IP .
+   * @default - The default security group of the instance, which only allows TCP 22 SSH ingress rule.
    */
   public readonly defaultSecurityGroup: ec2.ISecurityGroup; 
 
